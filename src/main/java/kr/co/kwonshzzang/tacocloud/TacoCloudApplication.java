df0554e7bbc1c1bfd1ac20fc6a1f40908a3c5e30
kwonshzzang <kwonshzzang@gmail.com>
2023-12-06 20:28:44 +0900

    First Commit

---
+++ src/main/java/kr/co/kwonshzzang/tacocloud/TacoCloudApplication.java
@@ -0,0 +1,13 @@
+package kr.co.kwonshzzang.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+}
