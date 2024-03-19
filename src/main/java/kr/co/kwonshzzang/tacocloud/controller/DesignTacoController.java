4d1786b4fd3731851e4f0271c35469954556fa5e
kwonshzzang <kwonshzzang@gmail.com>
2024-03-19 16:11:09 +0900

    Add Order Functions

--- src/main/java/kr/co/kwonshzzang/tacocloud/controller/DesignTacoController.java
+++ src/main/java/kr/co/kwonshzzang/tacocloud/controller/DesignTacoController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -38,6 +39,14 @@ public class DesignTacoController {
 
         model.addAttribute("taco", new Taco());
         return "design";
+    }
+
+    @PostMapping
+    public String processDesign(Taco design) {
+        // 이 지점에서 타코 디자인(선택된 식자재 내역)을 저장한다.
+        // 이 작업은 3장에서 할 것이다.
+        log.info("Processing design: " + design);
+        return "redirect:/orders/current";
     }
 
     private List<Ingredient> filterByType(
