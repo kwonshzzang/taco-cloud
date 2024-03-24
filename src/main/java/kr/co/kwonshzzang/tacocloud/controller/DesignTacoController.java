78c6cddf07a56a19518cf69756ce94ae93192d2c
kwonshzzang <kwonshzzang@gmail.com>
2024-03-24 20:54:14 +0900

    Add validation functions

--- src/main/java/kr/co/kwonshzzang/tacocloud/controller/DesignTacoController.java
+++ src/main/java/kr/co/kwonshzzang/tacocloud/controller/DesignTacoController.java
@@ -1,10 +1,12 @@
 package kr.co.kwonshzzang.tacocloud.controller;
 
+import jakarta.validation.Valid;
 import kr.co.kwonshzzang.tacocloud.domain.Ingredient;
 import kr.co.kwonshzzang.tacocloud.domain.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,7 +44,10 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(Taco design) {
+    public String processDesign(@Valid Taco design, Errors errors) {
+        if(errors.hasErrors()) {
+            return "design";
+        }
         // 이 지점에서 타코 디자인(선택된 식자재 내역)을 저장한다.
         // 이 작업은 3장에서 할 것이다.
         log.info("Processing design: " + design);
