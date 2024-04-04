c2fef5c34953e6f52a2a02f0e9fc849d1a879d79
kwonshzzang <kwonshzzang@gmail.com>
2024-04-04 18:43:06 +0900

    Add Repository

---
+++ src/main/java/kr/co/kwonshzzang/tacocloud/repository/JdbcIngredientRepository.java
@@ -0,0 +1,41 @@
+package kr.co.kwonshzzang.tacocloud.repository;
+
+import kr.co.kwonshzzang.tacocloud.domain.Ingredient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Repository
+@RequiredArgsConstructor
+public class JdbcIngredientRepository implements IngredientRepository {
+    private final JdbcTemplate jdbc;
+
+    @Override
+    public Iterable<Ingredient> findAll() {
+        return jdbc.query("SELECT id, name, type FROM Ingredient",
+                this::mapRowToIngredient);
+    }
+
+    @Override
+    public Ingredient findById(String id) {
+        return jdbc.queryForObject("SELECT id, name, type FROM Ingredient WHERE id=?",
+                this::mapRowToIngredient, id);
+    }
+
+    @Override
+    public Ingredient save(Ingredient ingredient) {
+        return null;
+    }
+
+    private Ingredient mapRowToIngredient(ResultSet rs, int rowNum)
+            throws SQLException {
+        return new Ingredient(
+            rs.getString("id"),
+            rs.getString("name"),
+            Ingredient.Type.valueOf(rs.getString("type"))
+        );
+    }
+}
