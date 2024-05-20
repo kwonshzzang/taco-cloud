ab38a23c48e6cf8226880b80fa435f69ceb9b170
kwonshzzang <kwonshzzang@gmail.com>
2024-05-20 15:54:17 +0900

    Add spring security config

---
+++ src/main/java/kr/co/kwonshzzang/tacocloud/security/SecurityConfig.java
@@ -0,0 +1,42 @@
+package kr.co.kwonshzzang.tacocloud.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        authorizationManagerRequestMatcherRegistry
+                                .requestMatchers("/design", "/orders").hasAnyRole("USER")
+                                .requestMatchers("/", "/**").permitAll()
+                                .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user1")
+                .password("{noop}password1")
+                .roles("USER")
+                .build());
+        manager.createUser(User.withUsername("user2")
+                .password("{noop}password2")
+                .roles("USER")
+                .build());
+
+        return manager;
+    }
+}
