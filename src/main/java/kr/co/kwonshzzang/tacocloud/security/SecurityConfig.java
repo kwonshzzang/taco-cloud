e6c44a188539b66f9d8fad897cf528c7a9564876
kwonshzzang <kwonshzzang@gmail.com>
2024-05-22 15:32:32 +0900

    Add spring security config

--- src/main/java/kr/co/kwonshzzang/tacocloud/security/SecurityConfig.java
+++ src/main/java/kr/co/kwonshzzang/tacocloud/security/SecurityConfig.java
@@ -1,18 +1,30 @@
 package kr.co.kwonshzzang.tacocloud.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig  {
+    private final DataSource dataSource;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -25,18 +37,47 @@ public class SecurityConfig  {
         return http.build();
     }
 
+    /**
+     * InMemory User Store
+     * @return UserDetails Manager
+     */
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        manager.createUser(User.withUsername("user1")
+//                .password("{noop}password1")
+//                .roles("USER")
+//                .build());
+//        manager.createUser(User.withUsername("user2")
+//                .password("{noop}password2")
+//                .roles("USER")
+//                .build());
+//        return manager;
+//    }
+
+    /**
+     * Jdbc User Store
+     * @return UserDetails Manager
+     */
     @Bean
     public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("user1")
-                .password("{noop}password1")
-                .roles("USER")
-                .build());
-        manager.createUser(User.withUsername("user2")
-                .password("{noop}password2")
-                .roles("USER")
-                .build());
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager();
+        userDetailsManager.setDataSource(dataSource);
+        userDetailsManager.setUsersByUsernameQuery("select username, password, enabled from users where username=?");
+        userDetailsManager.setAuthoritiesByUsernameQuery("select username, authority from authorities where username=?");
+        return userDetailsManager;
+    }
 
-        return manager;
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 }
